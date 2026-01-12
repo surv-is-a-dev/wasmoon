670b29557b6abc0ab5c5d344c4c9a81fa325e2ae
Miyo Sho <135030944+yuri-kiss@users.noreply.github.com>
2026-01-12 16:44:35 -0500

    nodejs build script is cooler ig

---
+++ wasmoon/build.cjs
@@ -0,0 +1,63 @@
+const path = require('path');
+const __dirname = path.resolve(path.dirname(__filename));
+
+const process = require('process');
+const { exec, execFile } = require('child_process');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const shellScript = `build.sh${isDev ? ' dev' : ''}`;
+
+function execAndLink(useExecFile, cmd_or_file, args_or_options, options_or_none) {
+  return new Promise((resolve, reject) => {
+    if (useExecFile) {
+      return execFile(cmd_or_file, args_or_options ?? [], options_or_none ?? {}, (error, stdout, stderr) => {
+        if (error) {
+          return reject(error);
+        }
+        stdout.pipe(process.stdout);
+        stderr.pipe(process.stderr);
+      }).on((exit_code) => process.exitCode = exit_code);
+    }
+    return exec(cmd_or_file, args_or_options ?? {}, (error, stdout, stderr) => {
+      if (error) {
+        return reject(error);
+      }
+      stdout.pipe(process.stdout);
+      stderr.pipe(process.stderr);
+    }).on((exit_code) => process.exitCode = exit_code);
+  });
+  stdout.pipe(process.stdout);
+}
+
+if ((process.env.ENV_WITH_DOCKER || '') == 0) {
+  execAndLink(
+    false,
+    path.join(__dirname, 'build.sh') + (isDev ? ' dev' : ''),
+    {
+      input: 'pipe',
+      cwd: __dirname,
+      shell: true,
+      windowsHide: true,
+    },
+  );
+} else {
+  execAndLink(
+    true,
+    'docker',
+    [
+      'run'
+      '--rm',
+      '-e', `ENV_WASM_NODEFS=${process.env.ENV_WASM_NODEFS == 0 ? '0' : '1'}`,
+      '-v', `${JSON.stringify(__dirname)}:/wasmoon`,
+      'emscripten/emsdk',
+      '/wasmoon/build.sh',
+    ].concat(isDev ? ['dev'] : []),
+    {
+      input: 'pipe',
+      cwd: __dirname,
+      shell: false,
+      windowsHide: true,
+    },
+  );
+}
